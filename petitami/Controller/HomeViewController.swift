601f540ff16306ee6192488e702fa1d7bee9bac8
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2021-09-29 10:35:40 -0300

    home view controller

---
+++ petitami/Controller/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  petitami
+//
+//  Created by Marcelo Simim on 29/09/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+    }
+}
